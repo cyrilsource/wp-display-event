79ef58b7949996302eac85d6f01b09305c17c680
cyrilsource <cyrilbron78@gmail.com>
2019-06-14 17:59:35 +0200

    Update page-event.php

--- page-event.php
+++ page-event.php
@@ -5,15 +5,14 @@
 
 get_header(); ?>
 
-	<main id="primary" class="site-main" role="main">
-  
-  <?php $query = getFutureEvent(); ?>
-			<h2>Future Events</h2>
-				<?php if($query->have_posts()) : while ($query->have_posts()) : $query->the_post();
+<main id="primary" class="site-main" role="main">
+	<?php $query = getFutureEvent(); ?>
+	<h2>Future Events</h2>
+	<?php if($query->have_posts()) : while ($query->have_posts()) : $query->the_post();
 					  
             //display future events
 					
-				<?php  endwhile; endif;wp_reset_query(); ?>
+	<?php  endwhile; endif;wp_reset_query(); ?>
         
 get_footer();
   
