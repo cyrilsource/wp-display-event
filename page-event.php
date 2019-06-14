45046d9b168422fd19f0097739a0d169cfffeac8
cyrilsource <cyrilbron78@gmail.com>
2019-06-14 17:58:21 +0200

    Create page-event.php

---
+++ page-event.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ template to display events
+ */
+
+get_header(); ?>
+
+	<main id="primary" class="site-main" role="main">
+  
+  <?php $query = getFutureEvent(); ?>
+			<h2>Future Events</h2>
+				<?php if($query->have_posts()) : while ($query->have_posts()) : $query->the_post();
+					  
+            //display future events
+					
+				<?php  endwhile; endif;wp_reset_query(); ?>
+        
+get_footer();
+  
+			
